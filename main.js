e5171b5ab005242a59d051ccc9ba52c2caa5cbb9
un1corn1205 <79351499+un1corn1205@users.noreply.github.com>
2021-10-24 17:43:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,9 +7,30 @@ function setup()
 {
     canvas = createCanvas(300,300);
     canvas.center();
+    video= createCapture(VIDEO);
+    video.hide();
+
+    poseNet= ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
 
 function draw()
 {
-    
+  image(video,0,0,300,300);
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+
+function modelLoaded(){
+    console.log("PoseNet is initialized");
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        console.log("NoseX= " + results[0].pose.nose.x);
+        console.log("NoseY= " + results[0].pose.nose.y);
+    }
 }
